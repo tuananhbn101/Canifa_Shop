c879928511185bb249e1dd8182b5a604b162caa0
Tuấn Anh <62342942+tuananhbn101@users.noreply.github.com>
2021-07-13 00:09:13 +0700

    Update content ui

--- app/src/main/java/com/example/canifa_shop/More/MoreFragment.java
+++ app/src/main/java/com/example/canifa_shop/More/MoreFragment.java
@@ -1,5 +1,6 @@
 package com.example.canifa_shop.More;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,7 +11,14 @@ import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
+import com.example.canifa_shop.Category.CategoryActivity;
+import com.example.canifa_shop.Customer.CustomerActivity;
+import com.example.canifa_shop.Manager.AcountManagerActivity;
+import com.example.canifa_shop.Manager.EmployeeManagerActivity;
+import com.example.canifa_shop.Product.ProductActivity;
 import com.example.canifa_shop.R;
+import com.example.canifa_shop.Unit.Object.Unit;
+import com.example.canifa_shop.Unit.UnitActivity;
 import com.example.canifa_shop.databinding.FragmentMoreBinding;
 
 public class MoreFragment extends Fragment {
@@ -29,6 +37,42 @@ public class MoreFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull @org.jetbrains.annotations.NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_more,container,false);
+        binding.btnAccountManager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), AcountManagerActivity.class));
+            }
+        });
+        binding.btnCategoryManager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), CategoryActivity.class));
+            }
+        });
+        binding.btnCustomerManager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), CustomerActivity.class));
+            }
+        });
+        binding.btnEmployeeManager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), EmployeeManagerActivity.class));
+            }
+        });
+        binding.btnProdutManager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), ProductActivity.class));
+            }
+        });
+        binding.btnUnitManager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), UnitActivity.class));
+            }
+        });
         return binding.getRoot();
     }
 }
